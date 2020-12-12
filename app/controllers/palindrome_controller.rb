0176845400a9572713009d3ab5808f96ec3f8403
magnus_test <magnus_test@test.com>
2020-12-12 20:15:48 +0300

    upgrade pal. controller

--- app/controllers/palindrome_controller.rb
+++ app/controllers/palindrome_controller.rb
@@ -1,24 +1,23 @@
+# frozen_string_literal: true
+
 class PalindromeController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def input
-  end
+  def input; end
 
   def view
-    val = check_input()
+    val = check_input
     if val
       if elem = PalindromeData.find_by(num: val)
         res = { count: elem.count, num: elem.num, nums: elem.nums, sq: elem.squared }
       else
         count = 1
-        res = [ [count, 1, 1] ]
+        res = [[count, 1, 1]]
         2.upto(val) do |num|
-          if palindrome? (num**2)
-            res.push [count += 1, num, num**2]
-          end
+          res.push [count += 1, num, num**2] if palindrome?(num**2)
         end
 
-        data = PalindromeData.new 
+        data = PalindromeData.new
         data.count = count
         data.num = val
         data.nums = res.map { |x| x[1] }.join(' ')
@@ -27,35 +26,36 @@ class PalindromeController < ApplicationController
         res = { count: data.count, num: data.num, nums: data.nums, sq: data.squared }
       end
     else
-      if is_numeric? params[:val]  
-        res = 'Error. Input is negavite number or 0.'
-      else
-        res = 'Error. Input is not a number.'
-      end
-    end  
-    @result = res   
+      res = if is_numeric? params[:val]
+              'Error. Input is negavite number or 0.'
+            else
+              'Error. Input is not a number.'
+            end
+    end
+    @result = res
   end
 
   def showDB
     elems = []
-    PalindromeData.all.each { |x| elems << {num: x.num, count: x.count, nums: x.nums, squared: x.squared} }
+    PalindromeData.all.each { |x| elems << { num: x.num, count: x.count, nums: x.nums, squared: x.squared } }
     render xml: elems.to_xml
   end
 
-  private 
-  def palindrome? num
+  private
+
+  def palindrome?(num)
     num == num.to_s.reverse.to_i
   end
 
-  def is_numeric? obj 
-    obj.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true
+  def is_numeric?(obj)
+    obj.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/)
   end
 
   def check_input
-    unless is_numeric? params[:val] and params[:val].to_i.positive?
-      return false
-    else 
-      return params[:val].to_i
+    if is_numeric?(params[:val]) && params[:val].to_i.positive?
+      params[:val].to_i
+    else
+      false
     end
   end
 end
